3b76e158b4df8f20983a18d64db48972b75c8515
SamuelPereiraBrandao <samuelpbrandao58@gmail.com>
2022-08-01 01:31:21 -0300

    add pags routerlink

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,6 +8,6 @@
 </template>
 <script>
 export default {
-    name: 'PagHome'
+    name: 'HomeVue'
 }
 </script>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,13 +1,38 @@
 <template>
-<div>
-     <h3>Login</h3>
-       <router-link :to="{path: '/home'}">Entrar - Home</router-link>
+    <div>
+        <nav class="navbar navbar-light bg-light menu-superior">
+            <div class="container">
+                <router-link class="nav-link" to="/">Empresa 360</router-link>
+                <div class="navbar-nav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Voltar</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
 
-</div>
-   
+        <div class="row mt-5">
+            <div class="col-6 offset-3">
+                <div class="card">
+                    <p class="card-header">Entrar</p>
+                    <div class="card-body">
+                        <div class="mb-3">
+                            <input type="email" class="form-control" placeholder="E-mail">
+                        </div>
+                        <div class="mb-3">
+                            <input type="password" class="form-control" placeholder="Senha">
+                        </div>
+                        <button class="btn btn-primary">Login</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 <script>
 export default {
-    name:'PagLogin'
+  name:'LoginVue'
 }
 </script>
