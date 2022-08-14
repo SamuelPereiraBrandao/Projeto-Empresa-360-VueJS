87353bd479c331ff8d98056356ea3ccc1f668106
SamuelPereiraBrandao <samuelpbrandao58@gmail.com>
2022-08-14 18:59:18 -0300

    update router

--- src/router.js
+++ src/router.js
@@ -117,5 +117,13 @@ const router = createRouter({
     history: createWebHashHistory(),
     routes //routes: routes
 })
+
+router.beforeEach((to, from) => {
+    console.log('origem:', to)
+    console.log('destino:', from)
+    // verificar se o usuario esta autorizado a utilizar aquela rota
+    console.log('metodo executado antes do acesso a rota destino')
+})
+
 export default router
 // --- fim rotas
