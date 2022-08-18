efc5dd6a8a1530ba907981a3b8bd8fedcd2ed40e
SamuelPereiraBrandao <samuelpbrandao58@gmail.com>
2022-08-18 10:55:07 -0300

    update

--- src/components/dashboard/Dashboard.vue
+++ src/components/dashboard/Dashboard.vue
@@ -1,84 +1,156 @@
 <template>
 
-    <div class="row mt-3 central">
-        <div class="col">
-            <h4>BENEFICIOS:</h4>
-            <ul>
-                <li>Beneficio 1</li>
-                <li>Beneficio 2</li>
-                <li>Beneficio 3</li>
-                <li>Beneficio 4</li>
-                <li>Beneficio 5</li>
-               
-            </ul>
+<div class="container py-5">
+  <!-- For Demo Purpose -->
+  <header class="text-center">
+    <h1 class="display-4 font-weight-bold">Empresa 360</h1>
+    <p class="font-italic text-muted mb-0">A melhor empresa, segundo estudos em 2022.</p>
+    <p class="font-italic">Acesse mais informações <a href="" class="text-muted">
+        <u>\360Studio/</u></a>
+    </p>
+  </header>
+  <div class="py-5">
+    <h3 class="font-weight-bold mb-0">360 Studio</h3>
+    <p class="font-italic text-muted mb-4">Nesta modalidade você consegue acessar todos os seus dados em um unico aplicativo, conseguindo gerenciar seus dados a qualquer momento e em qualquer lugar!</p>
+    <div class="row">
+      <!-- DEMO 1 Item-->
+      <div class="col-lg-6 mb-3 mb-lg-0">
+        <div class="hover hover-1 text-white rounded"><img src="https://img.freepik.com/fotos-gratis/homem-de-negocios-usando-aplicativos-de-celular-enviando-mensagens-de-texto-no-azul_496169-2223.jpg?w=996&t=st=1660829975~exp=1660830575~hmac=d2482145ebe95f8a9d83977283bcd1d6804e6a1e781431ca883c0919502cab3d" alt="">
+          <div class="hover-overlay"></div>
+          <div class="hover-1-content px-5 py-4">
+            <h3 class="hover-1-title text-uppercase font-weight-bold mb-0"> <span class="font-weight-light">360  </span>Studio</h3>
+            <p class="hover-1-description font-weight-light mb-0">O melhor aplicativo para gerenciamento de dados.</p>
+          </div>
         </div>
-        <div class="col">
-            <h4>CONTRAS:</h4>
-            <ul>
-                <li>contras 1</li>
-                <li>contras 2</li>
-                <li>contras 3</li>
-                <li>contras 4</li>
-                <li>contras 5</li>
-             
-            </ul>
+      </div>
+      <!-- DEMO 1 Item-->
+      <div class="col-lg-6">
+        <div class="hover hover-1 text-white rounded"><img src="https://img.freepik.com/fotos-gratis/vendas-bancarias-na-internet-empresario-africano-bem-sucedido-sentado-em-um-laptop-e-segurando-o-cartao-de-credito-na-mao-ate-o-empresario-fazer-pedidos-pela-internet_231208-701.jpg?w=996&t=st=1660829866~exp=1660830466~hmac=ca7b5aaba94af9e9db8858f28ea7b49efabfad96afb27a8720566bbd3fc3c576" alt="">
+          <div class="hover-overlay"></div>
+          <div class="hover-1-content px-5 py-4">
+            <h3 class="hover-1-title text-uppercase font-weight-bold mb-0"> <span class="font-weight-light">Cartão </span>360</h3>
+            <p class="hover-1-description font-weight-light mb-0">Com este cartão você tem vários beneficios que podem ser visualizados abaixo:</p>
+          </div>
         </div>
+      </div>
     </div>
+  </div>
 
-    <div class="texto">
-        <h1>Por que a <i>360</i> ?</h1>
+  <!-- DEMO 2-->
+  <div class="py-5">
+    <h3 class="font-weight-bold mb-0">Alguns Beneficios:</h3>
+
+    <div class="row">
+      <!-- DEMO 2 Item-->
+      <div class="col-lg-6 mb-3 mb-lg-0">
+        <div class="hover hover-2 text-white rounded"><img src="https://bootstrapious.com/i/snippets/sn-img-hover/hoverSet-2.jpg" alt="">
+          <div class="hover-overlay"></div>
+          <div class="hover-2-content px-5 py-4">
+            <h3 class="hover-2-title text-uppercase font-weight-bold mb-0"> <span class="font-weight-light">VIAGENS </span></h3>
+            <p class="hover-2-description text-uppercase mb-0">com nosso cartão você pode acumular pontos e trocar por viagens para onde você quiser!</p>
+          </div>
+        </div>
+      </div>
+
+      <!-- DEMO 2 Item-->
+      <div class="col-lg-6">
+        <div class="hover hover-2 text-white rounded"><img src="https://wallpaperaccess.com/full/6688071.jpg" alt="">
+          <div class="hover-overlay"></div>
+          <div class="hover-2-content px-5 py-4">
+            <h3 class="hover-2-title text-uppercase font-weight-bold mb-0"> <span class="font-weight-light"> hoteis</span></h3>
+            <p class="hover-2-description text-uppercase mb-0">E nessas viagens pode se utilizar os pontos para alugar qualquer hotel que deseja ficar!</p>
+          </div>
+        </div>
+      </div>
     </div>
-    <div id="content">
-        <ul class="cont">
-            <li>
-                <a href="">
-                    <img src="https://www.eusemfronteiras.com.br/wp-content/uploads/2019/07/46651047_l.jpg"
-                        class="attachment-thumbnail wp-post-image" alt="Imagem" title="" />
-                    <span>A  <i class="destaque">empresa 360</i>, te auxilia <br> a melhorar</span>
-                </a>
-            </li>
+  </div>
 
-            <li>
-                <a href="#">
-                    <img src="https://i1.wp.com/rumodafe.com.br/wp-content/uploads/2019/01/frases-religiosas-de-amizade.jpg?fit=1400%2C933&ssl=1"
-                        class="attachment-thumbnail wp-post-image" alt="Imagem" title="" />
-                    <span>Vamos te <i class="destaque">ajudar</i>, a <br>conseguir oque precisa!</span>
-                </a>
-            </li>
-
-            <li>
-                <a href="#">
-                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRmow8aI9hffp8Tgz4MxM1rdD78HwVHTI7enFOxIu23DR3EcF0U4xcr_J9Hb8tNsL2DuzI&usqp=CAU"
-                        class="attachment-thumbnail wp-post-image" alt="Imagem" title="" />
-                    <span>Construimos o seu <i class="destaque">futuro</i> no  <br>presente</span>
-                </a>
-            </li>
-
-            <li>
-                <a href="#">
-                    <img src="https://esbrasil.com.br/wp-content/uploads/2017/11/ainda-sobre-amizade.jpg"
-                        class="attachment-thumbnail wp-post-image" alt="Imagem" title="" />
-                    <span>Podemos fazer  <i class="destaque">melhor</i> <br>do que sua atual empresa!</span>
-                </a>
-            </li>
-            <li>
-                <a href="#">
-                    <img src="https://esbrasil.com.br/wp-content/uploads/2017/11/ainda-sobre-amizade.jpg"
-                        class="attachment-thumbnail wp-post-image" alt="Imagem" title="" />
-                    <span>Podemos fazer  <i class="destaque">melhor</i> <br>do que sua atual empresa!</span>
-                </a>
-            </li>
-            <li>
-                <a href="#">
-                    <img src="https://esbrasil.com.br/wp-content/uploads/2017/11/ainda-sobre-amizade.jpg"
-                        class="attachment-thumbnail wp-post-image" alt="Imagem" title="" />
-                    <span>Podemos fazer  <i class="destaque">melhor</i> <br>do que sua atual empresa!</span>
-                </a>
-            </li>
-
-        </ul>
-    </div>
+  <!-- DEMO 3 -->
+  <div class="py-5">
+    <h3 class="font-weight-bold mb-0">Acrescimos</h3>
     
+    <div class="row">
+      <!-- DEMO 3 Item-->
+      <div class="col-lg-6 mb-3 mb-lg-0">
+        <div class="hover hover-3 text-white rounded"><img src="https://bootstrapious.com/i/snippets/sn-img-hover/hoverSet-5.jpg" alt="">
+          <div class="hover-overlay"></div>
+          <div class="hover-3-content px-5 py-4">
+            <h3 class="hover-3-title text-uppercase font-weight-bold mb-1"><span class="font-weight-light">Acrescimos</span></h3>
+            <br>
+            <p class="hover-3-description small text-uppercase mb-0">O acrescimo foi o nome dado a esse beneficio, como funciona? <br> Então o acrescimo é nada mais nada menos quando você está devendo um valor, e a data de vencimento está chegando, nós daremos até 2 semanas para você nos pagar, e só depois das 2 semmanas será cobrado tal valor!</p>
+          </div>
+        </div>
+      </div>
+      <!-- DEMO 3 Item    -->
+      <div class="col-lg-6">
+        <div class="hover hover-3 text-white rounded"><img src="https://bootstrapious.com/i/snippets/sn-img-hover/hoverSet-6.jpg" alt="">
+          <div class="hover-overlay"></div>
+          <div class="hover-3-content px-5 py-4">
+            <h3 class="hover-3-title text-uppercase font-weight-bold mb-1"><span class="font-weight-light">Image </span>Caption</h3>
+            <p class="hover-3-description small text-uppercase mb-0">Lorem ipsum dolor sit amet, consectetur <br>adipisicing elit.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!-- DEMO 4 -->
+  <div class="py-5">
+    <h3 class="font-weight-bold mb-0">Demo 4</h3>
+    <p class="font-italic text-muted mb-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+    <div class="row">
+      <!-- DEMO 4 Item-->
+      <div class="col-lg-6 mb-3 mb-lg-0">
+        <div class="hover hover-4 text-white rounded"><img src="https://bootstrapious.com/i/snippets/sn-img-hover/hoverSet-7.jpg" alt="">
+          <div class="hover-overlay"></div>
+          <div class="hover-4-content">
+            <h3 class="hover-4-title text-uppercase font-weight-bold mb-0"><span class="font-weight-light">Image </span>Caption</h3>
+            <p class="hover-4-description text-uppercase mb-0 small">Lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod tempor incididunt</p>
+          </div>
+        </div>
+      </div>
+      <!-- DEMO 4 Item            -->
+      <div class="col-lg-6">
+        <div class="hover hover-4 text-white rounded"><img src="https://bootstrapious.com/i/snippets/sn-img-hover/hoverSet-8.jpg" alt="">
+          <div class="hover-overlay"></div>
+          <div class="hover-4-content">
+            <h3 class="hover-4-title text-uppercase font-weight-bold mb-0"><span class="font-weight-light">Image </span>Caption</h3>
+            <p class="hover-4-description text-uppercase mb-0 small">Lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod tempor incididunt</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+  <!-- DEMO 5 -->
+  <div class="py-5">
+    <h3 class="font-weight-bold mb-0">Demo 5</h3>
+    <p class="font-italic text-muted mb-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+    <div class="row">
+      <!-- DEMO 5 Item-->
+      <div class="col-lg-6 mb-3 mb-lg-0">
+        <div class="hover hover-5 text-white rounded"><img src="https://bootstrapious.com/i/snippets/sn-img-hover/hoverSet-10.jpg" alt="">
+          <div class="hover-overlay"></div>
+          <div class="hover-5-content">
+            <h3 class="hover-5-title text-uppercase font-weight-light mb-0">Image <strong class="font-weight-bold text-white">Caption </strong><span>Colorfull</span></h3>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-6">
+        <!-- DEMO 5 Item-->
+        <div class="hover hover-5 text-white rounded"><img src="https://bootstrapious.com/i/snippets/sn-img-hover/hoverSet-9.jpg" alt="">
+          <div class="hover-overlay"></div>
+          <div class="hover-5-content">
+            <h3 class="hover-5-title text-uppercase font-weight-light mb-0">Image <strong class="font-weight-bold text-white">Caption </strong><span>Colorfull</span></h3>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+</div>
+
 </template>
 
 <script>
@@ -89,133 +161,294 @@ export default {
 </script>
 <style scoped>
 
-h1 {
-    text-align: center;
-}
-*{
-        font-family:'Segoe UI', Tahoma, Geneva, Verdana, sans-serif
-
-}
-i {
-    text-decoration: none;
-    color:#0D6EFD;
-}
-li {
-    list-style: none;
+.hover {
+  overflow: hidden;
+  position: relative;
+  padding-bottom: 60%;
 }
 
-img {
-    flex-direction: column;
-    height: 200px;
-    background: center;
-    border-radius: 10px;
-
-}
-.texto{
-    padding: 50px 0px 50px 0px;
-}
-
-.texto :hover {
-    opacity: 1;
+.hover-overlay {
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: 90;
+  transition: all 0.4s;
 }
 
-body {
-    font: normal 14px Optima, Arial, sans-serif;
-
-    
+.hover img {
+  width: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  transition: all 0.3s;
 }
 
-/* Fonte normal tamanho 14 tenho Tahoma como fonte primaria */
-a {
-    text-decoration: none;
-    
-}
-
-/* Links sem efeitos */
-
-/* Carecteristicas da div. */
-#content {
-    padding-bottom: 100px;
-    border-radius: 10px;
-}
-
-/* Carecteristicas de tudo que esta dentro da Div e com Link */
-#content ul li a {
-    list-style: none;
-    display: block;
-    position: relative;
-    font-size: 12px;
-    margin: auto;
-    padding: 10px;
-    
-    display: flex;
-
-}
-
-/* Efeitos do texto */
-#content ul li a span {
-    display: block;
-    padding: 10px;
-    color: white;
-    position: absolute;
-    bottom: 0;
-    -webkit-border-radius: 0 0 5px 5px;
-    -moz-border-radius: 0 0 5px 5px;
-    border-radius: 0 0 5px 5px;
-    opacity: 0;
-    -moz-opacity: 0;
-    filter: alpha(opacity=0);
-    font-size: 25px;
-}
-
-/* Efeitos do texto quando o mouse estive em cima da Imagem */
-#content ul li:hover span {
-    border: 1px solid f00;
-    opacity: 1;
-    -moz-opacity: 1;
-    filter: alpha(opacity=100);
+.hover-content {
+  position: relative;
+  z-index: 99;
 }
 
 
-
-
-#content ul li a span {
-    /* Espaço da mensagem que ira aparecer */
-    padding-bottom: 20px;
-    padding-left: 20px;
+/* DEMO 1 ============================== */
+.hover-1 img {
+  width: 105%;
+  position: absolute;
+  top: 0;
+  left: -5%;
+  transition: all 0.3s;
 }
 
-ul {
-    text-decoration: none;
-    
+.hover-1-content {
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  z-index: 99;
+  transition: all 0.4s;
 }
 
-html, body, ul {     
-  position:relative; 
-  margin:0;           
-  padding:0;
-  width:100%;         
-  height:100%; 
+.hover-1 .hover-overlay {
+  background: rgba(0, 0, 0, 0.5);
+}
+
+.hover-1-description {
+  transform: translateY(0.5rem);
+  transition: all 0.4s;
+  opacity: 0;
+}
+
+.hover-1:hover .hover-1-content {
+  bottom: 2rem;
+}
+
+.hover-1:hover .hover-1-description {
+  opacity: 1;
+  transform: none;
+}
+
+.hover-1:hover img {
+  left: 0;
+}
+
+.hover-1:hover .hover-overlay {
+  opacity: 0;
 }
 
 
-.cont li {
-  display:block;
-  list-style-type: none;    
-  margin:0; 
-       
-  box-sizing: border-box; 
-  height:25%; 
+/* DEMO 2 ============================== */
+.hover-2 .hover-overlay {
+  background: linear-gradient(to top, rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.1));
 }
 
-.cont li:nth-child(odd) { 
-  clear:both; 
-  float:left;                             
+.hover-2-title {
+  position: absolute;
+  top: 50%;
+  left: 0;
+  text-align: center;
+  width: 100%;
+  z-index: 99;
+  transition: all 0.3s;
+}
 
+.hover-2-description {
+  width: 100%;
+  position: absolute;
+  bottom: 0;
+  opacity: 0;
+  left: 0;
+  text-align: center;
+  z-index: 99;
+  transition: all 0.3s;
 }
-.cont img :hover{
-    filter: blur(10px);
+
+.hover-2:hover .hover-2-title {
+  transform: translateY(-1.5rem);
 }
+
+.hover-2:hover .hover-2-description {
+  bottom: 0.5rem;
+  opacity: 1;
+}
+
+.hover-2:hover .hover-overlay {
+  background: linear-gradient(to top, rgba(0, 0, 0, 0.9), rgba(0, 0, 0, 0.1));
+}
+
+
+/* DEMO 3 ============================== */
+.hover-3::after {
+  content: '';
+  width: calc(100% - 3rem);
+  height: calc(100% - 3rem);
+  border: 1px solid #0D6EFD;
+  position: absolute;
+  top: 1.5rem;
+  left: 1.5rem;
+  z-index: 90;
+  transition: all 0.3s;
+  transform: scale(1.1);
+  opacity: 0;
+  display: block;
+  opacity: 0;
+}
+
+.hover-3-content {
+  position: absolute;
+  top: 50%;
+  left: 0;
+  width: 100%;
+  transform: translateY(-50%);
+  text-align: center;
+  z-index: 99;
+}
+
+.hover-3-description {
+  opacity: 0;
+  transform: scale(1.3);
+  transition: all 0.3s;
+}
+
+.hover-3 img {
+  width: 110%;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+
+.hover-3 .hover-overlay {
+  background: rgba(0, 0, 0, 0.2);
+}
+
+.hover-3:hover img {
+  width: 100%;
+}
+
+.hover-3:hover::after {
+  opacity: 1;
+  transform: none;
+}
+
+.hover-3:hover .hover-3-description {
+  opacity: 1;
+  transform: none;
+}
+
+.hover-3:hover .hover-overlay {
+  background: rgba(0, 0, 0, 0.8);
+}
+
+
+/* DEMO 4 ============================== */
+.hover-4 img {
+  width: 110%;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+
+.hover-4 .hover-overlay {
+  background: rgba(0, 0, 0, 0.4);
+  z-index: 90;
+}
+
+.hover-4-title {
+  position: absolute;
+  bottom: 0;
+  right: 0;
+  padding: 3rem;
+  z-index: 99;
+}
+
+.hover-4-description {
+  position: absolute;
+  top: 2rem;
+  left: 2rem;
+  text-align: right;
+  border-right: 3px solid #fff;
+  padding: 0 1rem;
+  z-index: 99;
+  transform: translateX(-1.5rem);
+  opacity: 0;
+  transition: all 0.3s;
+}
+
+@media (min-width: 992px) {
+  .hover-4-description {
+    width: 50%;
+  }
+}
+
+.hover-4:hover img {
+  width: 100%;
+}
+
+.hover-4:hover::after {
+  opacity: 1;
+  transform: none;
+}
+
+.hover-4:hover .hover-4-description {
+  opacity: 1;
+  transform: none;
+}
+
+.hover-4:hover .hover-overlay {
+  background: rgba(0, 0, 0, 0.8);
+}
+
+
+/* DEMO 5 ============================== */
+.hover-5::after {
+  content: '';
+  width: 100%;
+  height: 10px;
+  background: #0D6EFD;
+  position: absolute;
+  bottom: -10px;
+  left: 0;
+  display: block;
+  transition: all 0.3s;
+  z-index: 999;
+}
+
+.hover-5 .hover-overlay {
+  background: rgba(0, 0, 0, 0.4);
+}
+
+.hover-5-title {
+  position: absolute;
+  bottom: 1rem;
+  left: 0;
+  transition: all 0.3s;
+  padding: 2rem 3rem;
+  z-index: 99;
+}
+
+.hover-5-title span {
+  transition: all 0.4s;
+  opacity: 0;
+  color: #0D6EFD;
+}
+
+.hover-5:hover .hover-overlay {
+  background: rgba(0, 0, 0, 0.8);
+}
+
+.hover-5:hover .hover-5-title {
+  bottom: 0;
+}
+
+.hover-5:hover .hover-5-title span {
+  opacity: 1;
+}
+
+.hover-5:hover::after {
+  bottom: 0;
+}
+
+
+
 </style>
 
 
